a0d242824a25942c66c3a4bc615f376d8e306f2b
sergeyleon <sergey@balticit.ru>
2012-08-27 14:34:36 +0300

    Update mailer/sen_service.php
    fix maail body

--- mailer/sen_service.php
+++ mailer/sen_service.php
@@ -7,29 +7,29 @@ function ExecuteNonReturnQuery($query, $dbLink) {
 
 function getMonth($month) {
     switch ($month) {
-        case 1: $month = "января";
+        case 1: $month = "пїЅпїЅпїЅпїЅпїЅпїЅ";
             break;
-        case 2 : $month = "февраля";
+        case 2 : $month = "пїЅпїЅпїЅпїЅпїЅпїЅпїЅ";
             break;
-        case 3 : $month = "марта";
+        case 3 : $month = "пїЅпїЅпїЅпїЅпїЅ";
             break;
-        case 4 : $month = "апреля";
+        case 4 : $month = "пїЅпїЅпїЅпїЅпїЅпїЅ";
             break;
-        case 5: $month = "мая";
+        case 5: $month = "пїЅпїЅпїЅ";
             break;
-        case 6 : $month = "июня";
+        case 6 : $month = "пїЅпїЅпїЅпїЅ";
             break;
-        case 7 : $month = "июля";
+        case 7 : $month = "пїЅпїЅпїЅпїЅ";
             break;
-        case 8 : $month = "августа";
+        case 8 : $month = "пїЅпїЅпїЅпїЅпїЅпїЅпїЅ";
             break;
-        case 9 : $month = "сентября";
+        case 9 : $month = "пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ";
             break;
-        case 10 : $month = "октября";
+        case 10 : $month = "пїЅпїЅпїЅпїЅпїЅпїЅпїЅ";
             break;
-        case 11 : $month = "ноября";
+        case 11 : $month = "пїЅпїЅпїЅпїЅпїЅпїЅ";
             break;
-        case 12 : $month = "декабря";
+        case 12 : $month = "пїЅпїЅпїЅпїЅпїЅпїЅпїЅ";
             break;
     }
     return $month;
@@ -60,16 +60,16 @@ function generateHeader($username = "user") {
 			</div>
 		</tr>
 		<tr>
-		<td style='padding-top:10px;color:black;padding-left:10px' colspan='2'>Здравствуйте, $username!<br/>
-		За период с $start_date $strmonthstart по $day_end $strmonthend $year года на портале eventcatalog.ru добавились:
+		<td style='padding-top:10px;color:black;padding-left:10px' colspan='2'>пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ, $username!<br/>
+		пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅ $start_date $strmonthstart пїЅпїЅ $day_end $strmonthend $year пїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ eventcatalog.ru пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ:
 		</td>
 		</tr>";
 }
 
 function generateFooter() {
-    return "<tr><td style='padding-top:5px;color:black;text-decoration:none;padding-left:10px' colspan='2'>Удачной Вам event-недели и до встречи на страницах сайта!<br/><br/>
-    С уважением,<br/>
-    Ваш ежедневный помощник<br/>
+    return "<tr><td style='padding-top:5px;color:black;text-decoration:none;padding-left:10px' colspan='2'>пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ event-пїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ!<br/><br/>
+    пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ,<br/>
+    пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ<br/>
     <a href='http://www.eventcatalog.ru'>Eventcatalog.ru</a><br/>
     <br/>
     <p style='margin: 10px 0 0'>
@@ -83,7 +83,7 @@ function generateFooter() {
         <img src='http://eventcatalog.ru/images/yandex.png'>
     </a></p>
     <p style='color:gray;font-size:8pt;'>
-        Если Вы не желаете получать рассылку от портала для организаторов мероприятий, перейдите по <a style='color:gray;' href='http://eventcatalog.ru/u_cabinet'>этой ссылке</a>.
+        пїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ, пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ <a style='color:gray;' href='http://eventcatalog.ru/u_cabinet'>пїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ</a>.
     </p
     </td>
     </tr>";
@@ -126,10 +126,10 @@ function renderNewResident($doc, $color, $name, $type) {
 			</td>
 			<td style='padding-left: 10px;padding-top:5px;' valign='top'>
                             <span style='color:$color'>$name: <a style='text-decoration:none;color:$color' href='$base/$type/$title_url'>$title</a></span><br>
-				<span style='color:black'>Город: $city_name</span><br>
+				<span style='color:black'>пїЅпїЅпїЅпїЅпїЅ: $city_name</span><br>
                             <div style='position:relative; width: 700px; overflow:hidden; height:50px; z-index:0; margin-right: 5px; color:black;'>
                                 $text<br/>
-                                <a href='$base/$type/$title_url' style='color: blue;text-decoration:none;'>Перейти на страницу компании</a>
+                                <a href='$base/$type/$title_url' style='color: blue;text-decoration:none;'>пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ</a>
                             </div>
 			</td>
             </tr>";
@@ -164,6 +164,15 @@ function renderResidentNews($doc, $linkText) {
 }
 
 function prepareBody() {
+define("MYSQL_HOST", "mysql.baze.eventcatalog.ru.postman.ru");
+define("MYSQL_USER", "root");
+define("MYSQL_DATABASE", "eventcatalog_ru");
+define("MYSQL_PASSWORD", "2ygMPCBrm8");
+define("MYSQL_CHARSET", "cp1251");
+define("MYSQL_PORT", "63627");
+define("BASEURL", "http://eventcatalog.ru");	
+	
+	
     $dbLink = mysql_connect(MYSQL_HOST . ":" . MYSQL_PORT, MYSQL_USER, MYSQL_PASSWORD) or die("Could not connect: " . mysql_error());
     $filter = " AND DATEDIFF(NOW(),registration_date)<4";
     mysql_select_db(MYSQL_DATABASE, $dbLink) or die('Can\'t use db : ' . mysql_error());
@@ -184,10 +193,10 @@ function prepareBody() {
           case when DATEDIFF(NOW(),registration_date)<10 then 1 else 0 end is_new
         from tbl__eventtv_doc where active = 1" . $filter . " order by registration_date desc", $dbLink);
     if (count($publics) != 0) {
-        $html .="<tr><td colspan='2' style='color:black;padding-top:10px;font-weight:bold;padding-left:10px;'>НОВОЕ ВИДЕО НА EVENT TV:</td></tr>";
+        $html .="<tr><td colspan='2' style='color:black;padding-top:10px;font-weight:bold;padding-left:10px;'>пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅ EVENT TV:</td></tr>";
         foreach ($publics as $doc) {
             $doc['title_url'] = '/eventtv/' . $doc['title_url'];
-            $html .= renderItem($doc, "Смотреть репортаж");
+            $html .= renderItem($doc, "пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ");
         }
     }
     //Eventoteka array
@@ -197,20 +206,20 @@ function prepareBody() {
                   case when DATEDIFF(NOW(),registration_date)<10 then 1 else 0 end is_new
                   from tbl__public_doc where active = 1" . $filter . " order by registration_date desc", $dbLink);
     if (count($eteka) != 0) {
-        $html.="<tr><td colspan='2' style='color:black;padding-top:10px;font-weight:bold;padding-left:10px;'>НОВЫЕ СТАТЬИ В ЭВЕНТОТЕКЕ:</td></tr>";
+        $html.="<tr><td colspan='2' style='color:black;padding-top:10px;font-weight:bold;padding-left:10px;'>пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ:</td></tr>";
         foreach ($eteka as $doc) {
             $doc['title_url'] = '/book/' . $doc['title_url'];
-            $html .=renderItem($doc, "Читать статью полностью");
+            $html .=renderItem($doc, "пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ");
         }
     }
     //Industry news
     $filter2 = " AND DATEDIFF(NOW(),creation_date)<4";
     $industry = executeQuery("select `title`,`creation_date` as registration_date,`tbl_obj_id`, s_image as logo_image, annotation from `tbl__news` where `active`=1 AND creation_date IS NOT NULL" . $filter2, $dbLink);
     if (count($industry) != 0) {
-        $html.="<tr><td colspan='2' style='color:black;padding-top:10px;font-weight:bold;padding-left:10px;'>НОВОСТИ ИНДУСТРИИ:</td></tr>";
+        $html.="<tr><td colspan='2' style='color:black;padding-top:10px;font-weight:bold;padding-left:10px;'>пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ:</td></tr>";
         foreach ($industry as $doc) {
             $doc['title_url'] = '/news/details' . $doc['tbl_obj_id'] . '/';
-            $html.=renderItem($doc, "Читать новость полностью");
+            $html.=renderItem($doc, "пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ");
         }
     }
 
@@ -218,7 +227,7 @@ function prepareBody() {
     $filter3 = " AND DATEDIFF(NOW(),rn.date)<4";
     $res_news = executeQuery("select rn.*, DATE_FORMAT(date,'%d.%m.%y') as `strdate` from `tbl__resident_news` rn where rn.`active`=1" . $filter3, $dbLink);
     if (count($res_news) != 0) {
-        $html.="<tr><td colspan='2' style='color:black;padding-top:10px;font-weight:bold;padding-left:10px;'>НОВОСТИ РЕЗИДЕНТОВ:</td></tr>";
+        $html.="<tr><td colspan='2' style='color:black;padding-top:10px;font-weight:bold;padding-left:10px;'>пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ:</td></tr>";
         foreach ($res_news as $key => $val) {
             $res = ExecuteQuery("SELECT * FROM tbl__" . $res_news[$key]["resident_type"] . "_doc WHERE tbl_obj_id=" . $res_news[$key]["resident_id"], $dbLink);
             $res_news[$key]["title_url"] = $res[0]['title_url'];
@@ -231,22 +240,22 @@ function prepareBody() {
             $res_news[$key]["title"] = $res_news[$key]["title"];
             $res_news[$key]["text"] = strip_tags(substr($res_news[$key]["text"], 0, 100) . "...");
             switch ($val['resident_type']) {
-                case 'area': $res_news[$key]['sub'] = 'Новость площадки';
+                case 'area': $res_news[$key]['sub'] = 'пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ';
                     $res_news[$key]['color'] = "#39F";
                     break;
-                case 'artist': $res_news[$key]['sub'] = 'Новость артиста';
+                case 'artist': $res_news[$key]['sub'] = 'пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ';
                     $res_news[$key]['color'] = "#F06";
                     break;
-                case 'contractor': $res_news[$key]['sub'] = 'Новость подрядчика';
+                case 'contractor': $res_news[$key]['sub'] = 'пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ';
                     $res_news[$key]['color'] = "#F05620";
                     break;
-                case 'agency': $res_news[$key]['sub'] = 'Новость агентства';
+                case 'agency': $res_news[$key]['sub'] = 'пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ';
                     $res_news[$key]['color'] = "#9C0";
                     break;
             }
         }
         foreach ($res_news as $doc) {
-            $html.=renderResidentNews($doc, "Читать новость полностью");
+            $html.=renderResidentNews($doc, "пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ");
         }
     }
     //new_resident
@@ -255,34 +264,34 @@ function prepareBody() {
     $newAgency = executeQuery("select a.tbl_obj_id,a.logo_image,a.title,a.title_url,a.description,a.registration_date,c.title as city_name from tbl__agency_doc a left join tbl__city c on a.city = c.tbl_obj_id where a.active=1" . $filter, $dbLink);
     $newContractor = executeQuery("select a.tbl_obj_id,a.logo_image,a.title,a.title_url,a.description,a.registration_date,c.title as city_name from tbl__contractor_doc a left join tbl__city c on a.city = c.tbl_obj_id where a.active=1" . $filter, $dbLink);
     if (count($newArea) != 0 || count($newArtist) != 0 || count($newAgency) != 0 || count($newContractor) != 0) {
-        $html.="<tr><td colspan='2' style='color:black;padding-top:10px;font-weight:bold;padding-left:10px;'>НОВЫЕ РЕЗИДЕНТЫ:</td></tr>";
+        $html.="<tr><td colspan='2' style='color:black;padding-top:10px;font-weight:bold;padding-left:10px;'>пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ:</td></tr>";
         foreach ($newArea as $doc) {
             if (!isset($doc['logo'])) {
                 $doc['logo'] = $doc['logo_image'];
             }
             $doc['description'] = strip_tags(substr($doc['description'], 0, 150) . "...");
-            $html .= renderNewResident($doc, "#39F", "Площадка", "area");
+            $html .= renderNewResident($doc, "#39F", "пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ", "area");
         }
         foreach ($newArtist as $doc) {
             if (!isset($doc['logo'])) {
                 $doc['logo'] = $doc['logo_image'];
             }
             $doc['description'] = strip_tags(substr($doc['description'], 0, 150) . "...");
-            $html .= renderNewResident($doc, "#F06", "Артист", "artist");
+            $html .= renderNewResident($doc, "#F06", "пїЅпїЅпїЅпїЅпїЅпїЅ", "artist");
         }
         foreach ($newAgency as $doc) {
             if (!isset($doc['logo'])) {
                 $doc['logo'] = $doc['logo_image'];
             }
             $doc['description'] = strip_tags(substr($doc['description'], 0, 150) . "...");
-            $html .= renderNewResident($doc, "#9C0", "Агенство", "agency");
+            $html .= renderNewResident($doc, "#9C0", "пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ", "agency");
         }
         foreach ($newContractor as $doc) {
             if (!isset($doc['logo'])) {
                 $doc['logo'] = $doc['logo_image'];
             }
             $doc['description'] = strip_tags(substr($doc['description'], 0, 150) . "...");
-            $html .= renderNewResident($doc, "#F05620", "Подрядчик", "contractor");
+            $html .= renderNewResident($doc, "#F05620", "пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ", "contractor");
         }
     }
     mysql_close($dbLink);
@@ -320,7 +329,7 @@ $users = ExecuteQuery("select title,email from tbl__registered_user where subscr
 foreach($users as $doc){    
     $current_message =  $bodyStart . generateHeader($doc['title']) . $html;
     //send_mail
-    mail($doc['email'], "Свежие новости от портала eventcatalog.ru", $current_message, "Content-type: text/html; charset=window-1251 \r\n"
+    mail($doc['email'], "пїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅ eventcatalog.ru", $current_message, "Content-type: text/html; charset=window-1251 \r\n"
         . "From: EVENT NEWS <noreply@eventcatalog.ru> \r\n"
         . "X-Mailer: PHP/" . phpversion());
 }
