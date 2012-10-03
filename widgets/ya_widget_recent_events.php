be0bf27f3cd238fe947ca68d7e1bb30011a080b1
voldemar <voldemar.duketskiy@gmail.com>
2012-10-03 04:48:08 +0300

    вернул старое подключение к базе данных

--- widgets/ya_widget_recent_events.php
+++ widgets/ya_widget_recent_events.php
@@ -1,4 +1,5 @@
 <?php
+include 'db_connect.php';
 header('Content-Type: text/html; charset=utf-8');
 $limit = 28;
 
@@ -52,7 +53,7 @@ $(document).ready(function() {
 <?php
 $data = array();
 $new_data = array();
-$link = mysql_connect("localhost","widget","widget_password",true);
+#$link = mysql_connect("localhost","widget","widget_password",true);
 $sql = mysql_query('SELECT `title_url`, `logo_image`, `title`, UNIX_TIMESTAMP(`registration_date`) as `registration_date` FROM `tbl__agency_doc` WHERE `active` = 1 ORDER BY `registration_date` DESC LIMIT '.$limit.'');
 if (mysql_error()) die(mysql_error());
 WHILE ($r = mysql_fetch_assoc($sql)) {
