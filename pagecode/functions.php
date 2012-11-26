c89eb6f241f6957d037169a21a872101ed42b8ed
unknown <Sergey@.(none)>
2012-11-26 12:30:54 +0300

    fix 26.11.12 5

--- pagecode/functions.php
+++ pagecode/functions.php
@@ -138,8 +138,6 @@ function addDate($date){
 
 if($days > 7) { return "больше недели назад";}
 if($days > 1) { return declension($days,' день',' дня',' дней')." назад";} 
-if($days > 2 and $days < 3) { return "позавчера";}
-if($days > 1 and $days < 2) { return "вчера";}   
 if($days < 1) { return "сегодня";}
 
 }
