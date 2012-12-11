1b4a1b8c07cc355e277846ff95731ef1ed5ec776
unknown <Sergey@.(none)>
2012-12-11 15:37:05 +0300

    rating

---
+++ pagecode/pages/rating_agency.php
@@ -0,0 +1,82 @@
+<?php
+class rating_agency_php extends CPageCodeHandler
+{
+	public $pageSize = 100;
+
+	public function rating_agency_php()
+	{
+		$this->CPageCodeHandler();
+	}
+
+	public function PreRender()
+	{
+		$app = CApplicationContext::GetInstance();
+    $av_rwParams = array("page");
+		CURLHandler::CheckRewriteParams($av_rwParams);  
+		$rewriteParams = array();
+		$page = GP("page",1);
+		if (!is_numeric($page)||($page<1))
+		{
+			$page=1;
+		}
+		if ($page>1)
+		{
+			$rewriteParams["page"] = $page;
+		}		
+		$count = SQLProvider::ExecuteQuery("select count(1) quan from (
+																				select ul.to_resident_id as tbl_obj_id, c.title ,count(ul.tbl_obj_id) as voted
+                                            from tbl__userlike ul
+                                            join tbl__agency_doc c on c.tbl_obj_id = ul.to_resident_id and ul.to_resident_type='agency'
+                                            where c.active = 1
+                                            group by ul.to_resident_id, c.title) rate");
+		$count = $count[0]["quan"];
+		$pages = floor($count/$this->pageSize)+(($count%$this->pageSize==0)?0:1);
+		if (($page>$pages)&&($pages>0))
+		{
+			$page = $pages;
+			$rewriteParams["page"] = $page;
+			CURLHandler::Redirect(CURLHandler::$currentPath.CURLHandler::BuildRewriteParams($rewriteParams));
+		}
+		if ($page==1)
+		{
+			unset($rewriteParams["page"]);
+		}
+		
+		$rp = ($page-1)*$this->pageSize;
+		
+		
+		// Agencys list
+		$agencys = SQLProvider::ExecuteQuery("select ul.to_resident_id as tbl_obj_id, c.title ,count(ul.tbl_obj_id) as voted, c.title_url
+                                            from tbl__userlike ul
+                                            join tbl__agency_doc c on c.tbl_obj_id = ul.to_resident_id and ul.to_resident_type='agency'
+                                            where c.active = 1
+                                            group by ul.to_resident_id, c.title
+                                            order by voted desc, tbl_obj_id limit $rp,$this->pageSize");
+		$user = new CSessionUser("user");
+        CAuthorizer::AuthentificateUserFromCookie(&$user);
+        CAuthorizer::RestoreUserFromSession(&$user);
+        foreach ($agencys as $key=>&$agency) {
+			$agency["num"] = $rp+$key+1;
+			$agency["resident_type"] = 'agency';
+            if (!$user->authorized)
+                $agency["msg_vote"] = "onclick=\"javascript: ShowLikeMessage(); return false;\"";
+              else
+                $agency["msg_vote"] = "";
+		}
+		
+		$catList = $this->GetControl("rateList");
+		$catList->dataSource = $agencys;
+		
+		
+		
+		
+		//setting pager
+		$pager = $this->GetControl("pager");
+		$pager->currentPage = $page;
+		$pager->totalPages = $pages;
+		$pager->rewriteParams = $rewriteParams;
+		
+		
+	}
+}
+?>
---
+++ templates/rating_agency.php
@@ -0,0 +1,69 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+  <title>Рейтинг агенств - EVENT КАТАЛОГ</title>
+	<?php CRenderer::RenderControl("metadata"); ?>
+</head>
+<body>
+<table border="0" cellpadding="0" cellspacing="0" style="width:100%; height:100%;">
+<tr>
+  <td><?php CRenderer::RenderControl("topLine");?></td></tr>
+<tr><!-- Заголовок-->
+  <td style="padding-left: 30px; padding-right: 30px; padding-top: 16px;"><?php CRenderer::RenderControl("header"); ?></td>
+</tr>
+<tr><!--Меню-->
+  <td>
+    <?php CRenderer::RenderControl("menu"); ?>
+    <?php CRenderer::RenderControl("submenu"); ?>
+    <?php CRenderer::RenderControl("submenu1"); ?>
+    <?php CRenderer::RenderControl("submenu2"); ?>
+    <?php CRenderer::RenderControl("submenu3"); ?>
+    <?php CRenderer::RenderControl("submenu4"); ?>
+    <?php CRenderer::RenderControl("submenu5"); ?>
+  </td>
+</tr>
+<tr><!--содержание-->
+<td style="padding-left: 30px; padding-right: 30px; padding-top: 10px;">
+	<table width="100%" cellpadding="0" cellspacing="0" border="0">
+	<tr>
+		<td style="vertical-align:top; padding-left:5px; width:230px ">
+
+<table cellpadding="0" cellspacing="0" border="0" style="width: 218px; border: 1px solid #dadada;">
+  <tr>
+    <td class="ram5">
+      <div class="eventotekaMenu_title" style="margin: 0 0 0 0;">
+        <a class="black" href="/rating">Рейтинги компаний</a>
+      </div>
+      <div><a style="color:#000000; margin-left: 0;" class="leftml2 leftmgray" href="/rating">Все рейтинги</a></div>
+      <div><a style="color:#f05620; margin-left: 0;" class="leftml2 leftmgray" href="/rating/contractor">Рейтинг подрядчиков</a></div>
+      <div><a style="color:#3399ff; margin-left: 0;" class="leftml2 leftmgray" href="/rating/area">Рейтинг площадок</a></div>
+      <div><a style="color:#E3007B; margin-left: 0;" class="leftml2 leftmgray" href="/rating/artist">Рейтинг артистов</a></div>
+      <div class="selected"><a style="color:#99cc00; margin-left: 0;" class="leftml2 leftmgray" href="/rating/agency">Рейтинг агентств</a></div>
+    </td>
+  </tr>
+</table>
+
+
+		</td>
+		<td style="vertical-align:top;">
+			<div class="h3">Рейтинг агентств</div>
+			
+			<div style="padding-top:5px; ">
+			  
+  			<table border="0" cellpadding="0" cellspacing="0" width="100%"><?php CRenderer::RenderControl("rateList"); ?></table>
+  				
+			</div>
+			
+      <br />
+      <p><?php CRenderer::RenderControl("pager"); ?></p>
+		</td>
+    <?php require ROOTDIR.'templates/_leftMenuWitgets.php'; ?>
+	</tr>
+	</table>
+</td></tr>
+<tr><td class="partner_ban"><?php CRenderer::RenderControl("bottomBanners"); ?></td></tr>
+<tr><td class="foot"><?php CRenderer::RenderControl("footer"); ?></td></tr>
+</table>
+<?php CRenderer::RenderControl("googleanalytics"); ?>
+</body>
+</html>
