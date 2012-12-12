7d4e9d1cbae23c4f8cc040374ec71bc69ace2075
unknown <Sergey@.(none)>
2012-12-12 12:34:13 +0300

    fix title Arial 3

--- pagecode/functions.php
+++ pagecode/functions.php
@@ -136,12 +136,12 @@ function addDate($date){
     // минуты 60 = 60
     $minut = (int)(($sitetime - $years*31536000 - $months*2592000 - $days*86400 - $hour*3600)/60);
 
-
+var_dump($days.'/'.$hour);
 if($days > 7) { return "больше недели назад";}
 if($days > 2) { return declension($days,' день',' дня',' дней')." назад";} 
-if($days >= 2) { return "позавчера";}
-if($days = 1 ) { return "вчера";}
-if($days < 1 ) { return "сегодня";}
+if($days == 2) { return "позавчера";}
+if($days == 1 ) { return "вчера";}
+if($days == 0 ) { return "сегодня";}
 
 }
 
