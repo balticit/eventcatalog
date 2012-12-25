988ab704ea6d7d2284f7c4651a5bfad4fb03308f
unknown <Sergey@.(none)>
2012-12-25 13:26:24 +0300

    add thumb creator

--- pagecode/pages/contractor_details.php
+++ pagecode/pages/contractor_details.php
@@ -150,7 +150,8 @@ class contractor_details_php extends CPageCodeHandler
                   
                   $contractor["thumbs"] = '';
                   foreach ($thumbs as $thumb) {
-                    $contractor["thumbs"] .= '<li><span><img src="/upload/'.$thumb["m_image"].'" alt="" /></span></li>';
+                    $contractor["thumbs"] .= '<li><img src="/pagecode/classes/CreateThumb.php?src="/upload/'.$thumb["l_image"].'"&amp;h=200&amp;w=200&amp;zc=1" alt="" /></li>';
+                    //$contractor["thumbs"] .= '<li><span><img src="/upload/'.$thumb["m_image"].'" alt="" /></span></li>';
                   }
                   /* END ‘Œ“ » √¿À≈–≈» ¬ —œ»— ≈ */
                   
