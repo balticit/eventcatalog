019af01aef5cc05778e8ccf27cc7535cab743800
unknown <Sergey@.(none)>
2013-01-17 19:51:22 +0300

    fix link thumb 1

--- pagecode/pages/r_cabinet.php
+++ pagecode/pages/r_cabinet.php
@@ -2151,12 +2151,18 @@ $(function() {
 							$cab["main_area"].='<div class="message_history">';
 							$cab["main_area"].='<div class="header_message_history"><b>История сообщений</b></div>';
 							
+							$date = date("d M Y H:i",strtotime($reply_mess["time"]));
+              $date = str_ireplace($en_month,$ru_month,$date);
 							
-							$cab["main_area"].='<div class="message grey_message"><span style="float:right">'.$reply_mess["time"].'</span> <span style="margin-right:140px">'.ProcessMessage($reply_mess["text"])."</span></div>";
+							$cab["main_area"].='<div class="message grey_message"><span style="float:right">'.$date.'</span> <span style="margin-right:140px">'.ProcessMessage($reply_mess["text"])."</span></div>";
 							
   						foreach($h_mess as $mess) {
   						  if( $h_sender == $mess['sender_id']) { $class="grey_message"; } else { $class="white_message";}
-                  $cab["main_area"].='<div class="'.$class.' message"><span style="float:right">'.$reply_mess["time"].'</span> <span style="margin-right:140px">'.$mess["text"].'</span></div>';
+  						  
+  						  $date = date("d M Y H:i",strtotime($reply_mess["time"]));
+              $date = str_ireplace($en_month,$ru_month,$date);
+  						  
+                  $cab["main_area"].='<div class="'.$class.' message"><span style="float:right">'.$date.'</span> <span style="margin-right:140px">'.$mess["text"].'</span></div>';
               }
               $cab["main_area"].='</div>';
 										
@@ -2164,7 +2170,11 @@ $(function() {
 						}
 						elseif ($m_action=="view")
 						{
-						  $cab["main_area"].='<div style="color:#333333; background-color:#EEEEEE; padding:8px; margin:0 0 12px; -moz-border-radius: 6px 6px 6px 6px;"><span style="float:right">'.$reply_mess["time"].'</span>  <span style="margin-right:140px">'.ProcessMessage($reply_mess["text"])."</span></div>";
+						
+						  $date = date("d M Y H:i",strtotime($reply_mess["time"]));
+              $date = str_ireplace($en_month,$ru_month,$date);
+						
+						  $cab["main_area"].='<div style="color:#333333; background-color:#EEEEEE; padding:8px; margin:0 0 12px; -moz-border-radius: 6px 6px 6px 6px;"><span style="float:right">'.$date.'</span>  <span style="margin-right:140px">'.ProcessMessage($reply_mess["text"])."</span></div>";
 						
 							$cab["main_area"].='<form method="get" action="/r_cabinet/data/my_messages/action/delete/rid/'.$reply_mess["tbl_obj_id"].'/">
 										<input type="submit" value="Удалить" onClick="javascript:return confirm(\'Удалить данное сообщение\');"/><br/><br/><br/>
@@ -2255,8 +2265,8 @@ $(function() {
 
 						$cab["main_area"] .='<form id="fav_del_form" method="post"><table cellspacing=0 cellpadding=0 class="message_list">
 										<tr>
-									    <th>&nbsp;</th>
-									    <th>&nbsp;</th>
+									    
+		
 											<th>'.($m_action=="inbox"?"От":"Кому").'</th>
 											<th>Сообщение</th>
 											<th width="130">Дата</th>'.($m_action=="outbox"?"<th width='130'>Прочитано</th>":"").'
