e6cd4e4d5fbd8239d64b45c1976d9f9583d7e67d
unknown <Sergey@.(none)>
2013-01-21 12:14:39 +0300

    fix video url

--- pagecode/functions.php
+++ pagecode/functions.php
@@ -216,9 +216,9 @@ function get_video_id( $url ) {
    return $url;
   
   } elseif ( preg_match( '/watch/', $url, $matches) ) {
-   $arr = parse_url($url);
-   $url = str_replace( 'v=', '', $arr['query'] );
-   return $url;
+   //$arr = parse_url($url);
+   //$url = str_replace( 'v=', '', $arr['query'] );
+   return parse_yturl($url);
    
   } elseif ( preg_match( '/http:\/\/www.youtube.com\/v/', $url, $matches) ) {
    $arr = parse_url($url);
@@ -241,5 +241,24 @@ function get_video_id( $url ) {
   }
  }
 
+function parse_yturl($url) 
+{
+    $pattern = '#^(?:https?://)?';    # Optional URL scheme. Either http or https.
+    $pattern .= '(?:www\.)?';         #  Optional www subdomain.
+    $pattern .= '(?:';                #  Group host alternatives:
+    $pattern .=   'youtu\.be/';       #    Either youtu.be,
+    $pattern .=   '|youtube\.com';    #    or youtube.com
+    $pattern .=   '(?:';              #    Group path alternatives:
+    $pattern .=     '/embed/';        #      Either /embed/,
+    $pattern .=     '|/v/';           #      or /v/,
+    $pattern .=     '|/watch\?v=';    #      or /watch?v=,    
+    $pattern .=     '|/watch\?.+&v='; #      or /watch?other_param&v=
+    $pattern .=   ')';                #    End path alternatives.
+    $pattern .= ')';                  #  End host alternatives.
+    $pattern .= '([\w-]{11})';        # 11 characters (Length of Youtube video ids).
+    $pattern .= '(?:.+)?$#x';         # Optional other ending URL parameters.
+    preg_match($pattern, $url, $matches);
+    return (isset($matches[1])) ? $matches[1] : false;
+}
 
 ?>
